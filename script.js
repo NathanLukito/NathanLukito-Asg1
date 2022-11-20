466717742f82f50a4eaeb2687a57cab7f1e1d285
NathanLukito <S10244400@connect.np.edu.sg>
2022-11-20 22:01:03 +0800

    Finished with main page

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function nav_button() {
   document.getElementById('navigation').classList.toggle('active');
   document.getElementById('fade').classList.toggle('fade');
 }
+
+
+
+// let link = document.querySelectorAll(".nav_link");
+// link.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     let element = document.getElementById(item.getAttribute("data-link"));
+//     console.log(item);
+//     console.log(element);
+//     console.log(link)
+//     element.scrollIntoView(true);
+//   })})
+
+  
+
+
+
+
+
